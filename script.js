5c06ee57c0b449594917562b001d9408a2c7161c
ArdiSyla <100045418+ArdiSyla@users.noreply.github.com>
2024-07-08 22:47:18 +0200

    Added getHumanChoice function, this function prompts the user for their choice

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 function getComputerChoice() {
     const randomNum = Math.random();
-    if (randomNum < 0.33) {
+    if (randomNum < 0.30) {
         return "rock";
-    } else if (randomNum < 0.66) {
+    } else if (randomNum < 0.60) {
         return "paper";
     }else {
-        return "scsissors";
+        return "scissors";
     }
     
 }
 
 const ComputerChoice = getComputerChoice();
 console.log("Computer chose:", ComputerChoice);
+
+
+function getHumanChoice() {
+    const userChoice = prompt("Enter your choice(rock, paper or scissors):");
+    if  (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+        return userChoice;
+    }else {
+        console.log("Invalid choice. {Please enter rock, paper or scissors.");
+        return null;
+    }   
+}
+
+const humanChoice = getHumanChoice();
+if(humanChoice) {
+    console.log("You chose:", humanChoice);
+
+}
