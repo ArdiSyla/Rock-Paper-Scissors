6ca57f0a48fe30fcd401c8c56ee051ac61541e8c
ArdiSyla <100045418+ArdiSyla@users.noreply.github.com>
2024-07-08 23:38:37 +0200

    Added playRound function, this function determines the winner of a single round based on user and computer choices

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice() {
     const randomNum = Math.random();
-    if (randomNum < 0.30) {
+    if (randomNum < 0.35) {
         return "rock";
-    } else if (randomNum < 0.60) {
+    } else if (randomNum < 0.65) {
         return "paper";
     }else {
         return "scissors";
@@ -10,22 +10,50 @@ function getComputerChoice() {
     
 }
 
-const ComputerChoice = getComputerChoice();
-console.log("Computer chose:", ComputerChoice);
-
 
 function getHumanChoice() {
     const userChoice = prompt("Enter your choice(rock, paper or scissors):");
-    if  (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-        return userChoice;
+    if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors" ||
+        userChoice.toLowerCase() === "rock" || userChoice.toLowerCase() === "paper" || userChoice.toLowerCase() === "scissors") {
+        return userChoice.toLowerCase();
     }else {
         console.log("Invalid choice. {Please enter rock, paper or scissors.");
         return null;
     }   
 }
 
-const humanChoice = getHumanChoice();
-if(humanChoice) {
-    console.log("You chose:", humanChoice);
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice === computerChoice) {
+        console.log("It's a tie! Both chose " + humanChoice);
+    }  else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+    } else {
+        computerScore++;
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    }
+
+   
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+
 
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
